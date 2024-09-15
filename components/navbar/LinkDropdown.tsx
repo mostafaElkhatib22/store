22b75015152811c4571fda2121da617bf507cedc
mostafaElkhatib22 <mostafaelkhatib26@gmail.com>
2024-09-15 13:09:12 +0300

    add auth provider

--- components/navbar/LinkDropdown.tsx
+++ components/navbar/LinkDropdown.tsx
@@ -41,7 +41,6 @@ function LinkDropdown() {
             </SignInButton>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <SignOutLink />
         </SignedOut>
         <SignedIn>
           {links.map((link) => {
