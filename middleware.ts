93d5e4891d9e9fd9c3d205beed448d3a306c5cfb
mostafaElkhatib22 <mostafaelkhatib26@gmail.com>
2024-09-15 14:14:49 +0300

    add auth provider

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-const isPublicRoute = createRouteMatcher(["/", "/about"]);
-
+const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
+  if (!isPublicRoute(request)) auth().protect();
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
